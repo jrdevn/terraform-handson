fd8a8471582e348b4641ce5f30716848aa765b1a
jhonata.santos <jhonata.santos@semantix.ai>
2023-03-21 15:52:33 -0300

    provisioners aws

---
+++ aws-vm-provisioner/main.tf
@@ -0,0 +1,35 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "jhonata-remotestate-s3"
+    key    = "aws-vm-provisioners /terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner      = "jhonata"
+      managed_by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "jhonata-remotestate-s3"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
---
+++ aws-vm-provisioner/vm.tf
@@ -0,0 +1,53 @@
+resource "aws_key_pair" "key" {
+  key_name   = "aws-key"
+  public_key = file("./aws-key.pub")
+}
+
+resource "aws_instance" "vm" {
+  ami                         = "ami-02f3f602d23f1659d"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+
+  // local_exec 
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> public_ip.txt"
+  }
+
+  // file with content
+  provisioner "file" {
+    content     = "ip public: ${self.public_ip}"
+    destination = "/tmp/public_ip.txt"
+  }
+
+
+  // file with source
+  provisioner "file" {
+    source      = "./teste.txt"
+    destination = "/tmp/exemplo.txt"
+  }
+
+
+  // remote-exec
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./aws-key")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo ami: ${self.ami} >> /tmp/ami.txt",
+      "echo private_ip ${self.private_ip} >> /tmp/private_ip.txt",
+    ]
+  }
+
+  tags = {
+    Name = "vm-terraform"
+  }
+}
+
