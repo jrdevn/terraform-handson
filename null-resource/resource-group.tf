be0cdbcffb0db2be6035ced02d77e9a794c9e250
jhonata.santos <jhonata.santos@semantix.ai>
2023-03-27 15:56:07 -0300

    recursos extras pt1

---
+++ null-resource/resource-group.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "null-resource"
+  location = var.location
+  tags     = local.commont_tags
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "local-exec" {
+    command = "echo Resource Group ID ${azurerm_resource_group.resource_group.id} >> resource-group-id.txt"
+  }
+}
